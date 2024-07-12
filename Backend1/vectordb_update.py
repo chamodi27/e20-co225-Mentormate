0b4879ab13b534fb755d47df986b65f55f1d3b8e
Yohansenanayake <e20363@eng.pdn.ac.lk>
2024-07-12 18:34:35 +0530

    Added long lasting session indipendent chat history. yet to consider about the token window size

--- Backend1/vectordb_update.py
+++ Backend1/vectordb_update.py
@@ -6,7 +6,12 @@ from chroma_db_manager import ChromaDBManager
 # creating chromaDB client to access (add data to) the vector database
 vectorDB_client_pdf = ChromaDBManager(db_path="Backend1/vectorDb",collection_name="PDFCollection")
 # adding data to the vector database using chroma client
-vectorDB_client_pdf.add_data_to_vectorDb('Backend1/testData/IntroToBio.pdf')
-print("1 done")
-vectorDB_client_pdf.add_data_to_vectorDb('Backend1/testData/chapter2.pdf')
-print("2 done")
+#vectorDB_client_pdf.add_data_to_vectorDb('Backend1/testData/IntroToBio.pdf')
+#print("1 done")
+#vectorDB_client_pdf.add_data_to_vectorDb('Backend1/testData/chapter2.pdf')
+#print("2 done")
+
+vectorDB_client_pdf.add_data_to_vectorDb('Backend1/testData/BioResoBookGr12.pdf')
+print("Full book done")
+embeddings_count =vectorDB_client_pdf.collection.count()
+print("embeddings count",embeddings_count)
