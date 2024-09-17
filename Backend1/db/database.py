26a1d58117f515c39cb4e1f93e6afaa52f5b6831
Yohansenanayake <e20363@eng.pdn.ac.lk>
2024-09-17 11:38:09 +0530

    Database.py uses enviroment varibales to connect to database

--- Backend1/db/database.py
+++ Backend1/db/database.py
@@ -1,6 +1,8 @@
 import sys
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Update the path to include the parent directory of `Backend1`
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -8,8 +10,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.models import Base
 
+database_url = os.getenv('DATABASE_URL')
 # Setup the MySQL connection
-engine = create_engine('mysql+pymysql://root:@localhost/mentormate')
+engine = create_engine(database_url)
 
 # Create tables if they don't exist
 Base.metadata.create_all(engine)
